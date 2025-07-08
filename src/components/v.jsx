ecc6219ae3d1e0e62f5252f2017191d75fac851d
Ahmed Belmehnouf <128633214+Untitled-Master@users.noreply.github.com>
2025-07-08 05:21:19 +0200

    Update v.jsx

--- src/components/v.jsx
+++ src/components/v.jsx
@@ -12,7 +12,7 @@ const VisitCounter = () => {
     const fetchVisits = async () => {
       try {
         const response = await fetch(
-          "https://visitcounter-estinsrc.onrender.com/dashboard", 
+          "https://visitcounter-estinsrc.onrender.com/", 
           { signal }
         );
         const data = await response.json();
